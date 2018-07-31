824bd8ab7224f899c1e2a82a940e9e3d3d69d261
umamahesh5b2 <41734527+umamahesh5b2@users.noreply.github.com>
2018-07-31 14:49:03 +0530

    Update 9.java

--- 9.java
+++ 9.java
@@ -5,10 +5,8 @@ public class Array_Sum
     {
         int n,sum=0;
         Scanner s=new Scanner(System.in);
-        System.out.print("Enter no. of elements you want in array:");
         n=s.nextInt();
         int a[]=new int[n];
-        System.out.println("Enter all the elements:");
         for(int i=0;i<n;i++)
         {
             a[i]=s.nextInt();
